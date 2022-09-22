3adefe3dcafb68e6d5c784e43d3a7c2253dd4a63
charuzain <charuabhizain@gmail.com>
2022-09-22 19:36:13 -0400

    first commit

---
+++ Numbers/Numbers/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Numbers
+//
+//  Created by Charu Jain on 2022-09-22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
