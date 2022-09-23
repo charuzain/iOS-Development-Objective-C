49f9102eb03341e7f9815ce854ca29cd208415df
charuzain <charuabhizain@gmail.com>
2022-09-22 20:56:31 -0400

    Add 2 numbers

--- Numbers/Numbers/main.m
+++ Numbers/Numbers/main.m
@@ -9,8 +9,20 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        int num1;
+        int num2;
+        int result;
+        
+        NSLog(@" \n Enter first number");
+        scanf(" %i" , &num1);
+        
+        NSLog(@" \n Enter second number");
+        scanf("%i" ,&num2);
+        
+        result = num1 + num2;
+        
+        NSLog(@"\n The sum of %i and %i is %i", num1 , num2 , result);
     }
     return 0;
 }
