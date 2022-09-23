da9bb56e2c5a0b36f04f30c8daf86495c1bcd7b5
charuzain <charuabhizain@gmail.com>
2022-09-22 21:07:52 -0400

    Print message

---
+++ NSLog/NSLog/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSLog
+//
+//  Created by Charu Jain on 2022-09-22.
+//
+
+//Write a program to print "Hello all !! This is chapter 2
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int chapter = 2;
+        NSLog(@" \n Hello all !! This is chapter %i " , chapter);
+    
+    }
+    return 0;
+}
