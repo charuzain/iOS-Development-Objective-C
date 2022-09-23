1fcf24c7622ffac2093fe68f83d14da9b33ffc40
charuzain <charuabhizain@gmail.com>
2022-09-23 12:43:40 -0400

    Mutable array

---
+++ MutableArray/MutableArray/main.m
@@ -0,0 +1,43 @@
+//
+//  main.m
+//  MutableArray
+//
+//  Created by Charu Jain on 2022-09-23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+//      NSMutble array
+        // by init we initialize a empty array receTypes
+        NSMutableArray *raceTypes = [[NSMutableArray alloc]init];
+        
+        // Add item to this array
+        // to call method [instanceName instnaceMethod/function]
+        
+        [raceTypes addObject:@"Marathon"];
+        [raceTypes addObject:@"Half-Marathon"];
+        [raceTypes addObject:@"10k"];
+        [raceTypes addObject:@"Triathlon"];
+        
+        
+        //to remove last item in object
+        
+        [raceTypes removeLastObject];
+        
+        NSUInteger size =   [raceTypes count];
+        NSLog(@"%lu" , size);
+        
+        
+        // to loop
+        
+        for(NSString *raceType in raceTypes){
+            NSLog(@"\n \t %@" , raceType);
+        }
+        
+        
+        
+    }
+    return 0;
+}
