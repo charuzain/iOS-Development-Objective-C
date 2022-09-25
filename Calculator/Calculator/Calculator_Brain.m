1fe45b1c72e82b74f29498fa5c5974c6758e606a
charuzain <charuabhizain@gmail.com>
2022-09-24 21:48:34 -0400

    Implement pushItem method

--- Calculator/Calculator/Calculator_Brain.m
+++ Calculator/Calculator/Calculator_Brain.m
@@ -12,6 +12,7 @@
 @interface Calculator_Brain()
 
 @property (nonatomic , strong , readwrite) NSMutableArray *numbers;
+//-(double)popItem;
 @end
 
 
@@ -23,11 +24,15 @@
     }
     return _numbers;
 }
+// method with parameters -(returnType)methodName:(paramterType)ParameterName{}
+-(void)pushItem:(double)num{
+//An array can only store objects (more precise: pointers to object). So you have to create objects which store integer
+    NSNumber *number = [NSNumber numberWithDouble:num];
+    [self.numbers addObject:number];
+    //_ property variable itslef
+    // self. --> calling the property . When we are calling the propery that mean we are  calling the getter. Lazy loading means we instantiate the object and the getter . if we do [_numbers addObject:number] we are not doing lazy loading we are just calling the variable.
+    // whenever we access property within a class always use self unless we are in getter or init method
 
-
-
-
-
-
+}
 
 @end
