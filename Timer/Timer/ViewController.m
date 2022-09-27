49ec6b0a39bc73922623862f23e68aa80155c375
charuzain <charuabhizain@gmail.com>
2022-09-27 14:54:56 -0400

    Restart funcionality imlemented

--- Timer/Timer/ViewController.m
+++ Timer/Timer/ViewController.m
@@ -23,18 +23,29 @@
     
 }
 
-//NSTimer : Is a function we can trigger and  repeat something again and again. Evrytime it is triggered it perform a function or trigger an action
+
 
 - (IBAction)Restart:(id)sender {
+    
+    [timer invalidate];
+    count = 0;
+    self.label.text = [NSString stringWithFormat:@"%i" ,count];
+    
+    //restart the counter
+    
+    timer =  [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateTimer) userInfo:nil repeats:YES];
+    
+    
+    
 }
 
 - (IBAction)Pause:(id)sender {
     [timer invalidate];
-    
 
 }
 
 - (IBAction)Start:(id)sender {
+    //NSTimer : Is a function we can trigger and  repeat something again and again. Evrytime it is triggered it perform a function or trigger an action
     
     timer =  [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateTimer) userInfo:nil repeats:YES];
 }
